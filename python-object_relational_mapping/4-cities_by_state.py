6767eb3ba9fbd33dd56dccc17f92bd4c03cf5b11
H-Levison <h.levison@alustudent.com>
2024-12-01 07:17:52 -0800

    Python Object relational mapping

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,19 +1,28 @@
-#!/usr/bin/python3
-""" connects and executes an SQL query to retrieve data from the 'cities'
-and 'states' tables, joining them on the 'state_id' and ordering by 'cities.id'.
 """
+Select all records from states table
+"""
+from sys import argv
+
+import MySQLdb
 
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
+    username, password, database = argv[1:4]
+    # default host is 'localhost' and default port is '3306'
+    connection = MySQLdb.connect(
+        user=username,
+        password=password,
+        db=database
+    )
 
-    h, p = "localhost", 3306,
-    u, psw, db_name = argv[1], argv[2], argv[3]
-    with MySQLdb.connect(host=h, user=u, passwd=psw, db=db_name, port=p) as db:
-        with db.cursor() as cursor_obj:
-            query = """SELECT cities.id, cities.name, states.name FROM cities,
-            states WHERE cities.state_id = states.id ORDER BY cities.id ASC"""
-            cursor_obj.execute(query)
-            data = cursor_obj.fetchall()
-            for state in data:
-                print(state)
+    cursor = connection.cursor()
+    cursor.execute(
+        'SELECT cities.id, cities.name , states.name '
+        'FROM states INNER JOIN cities '
+        'ON states.id = cities.state_id '
+        'ORDER BY cities.id')
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    connection.close()
