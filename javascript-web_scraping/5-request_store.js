62e04a4af179879dd6bc227b22a2ee6f6d53d30b
H-Levison <h.levison@alustudent.com>
2025-02-12 23:58:23 -0800

    JS web Scripting

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,18 +1,4 @@
 #!/usr/bin/node
-const req = require('request');
-const url = process.argv[2];
-
-let count = 0;
-let data;
-req.get(url, (err, res) => {
-  if (err) console.log(err);
-  else {
-    data = JSON.parse(res.body).results;
-    data.forEach((obj) => {
-      obj.characters.forEach((character) => {
-        if (character.includes('/18/')) count++;
-      });
-    });
-  }
-  console.log(count);
-});
+const fs = require('fs');
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
